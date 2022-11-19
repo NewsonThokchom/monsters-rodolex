bfcff6f316349dc494ed05509580decaa2eae251
Newson Thokchom <newsonthokchom@gmail.com>
2022-11-19 13:16:13 +0530

    mapping arrays to elements

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ class App extends Component {
   constructor() {
     super();
 
+    // this.state = {
+    //   name: {
+    //     firstName: 'Newson',
+    //     lastName: 'Thokchom',
+    //   },
+    //   company: 'Lamzing Technologies Pvt.Ltd.'
+    // }
     this.state = {
-      name: {
-        firstName: 'Newson',
-        lastName: 'Thokchom',
-      },
-      company: 'Lamzing Technologies Pvt.Ltd.'
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        }
+      ]
+
     }
 
   }
@@ -19,32 +33,12 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-
-          <button
-            onClick={() => {
-
-              // first function is the updater function - passing a function that returns object i.e, name:{...},company:'..'
-              // second function is the callback function
-
-              // state variable is equal to the current state but second callback function is optional
-              // second function ie, callback function is going to run only after all of the state changes have been applied
-              this.setState((state, props) => {
-                return { name: { firstName: 'Lone', lastName: 'Coder' }, company: 'LoneCoder YT' }
-              }, () => {
-                console.log(this.state) // this will print the updated state
-              })
-
-              console.log(this.state) // this will print the current state
-
-            }}
-          >Change Name</button>
-        </header>
-      </div >
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
+      </div>
     );
   }
 
