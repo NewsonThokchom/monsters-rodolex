7b02f1fc5cbaacd31384f75a45e5e0b1e743c484
Newson Thokchom <newsonthokchom@gmail.com>
2022-11-20 23:47:42 +0530

    Optimized code by using object destructure and seperate onSerachChange function

--- src/App.js
+++ src/App.js
@@ -28,31 +28,37 @@ class App extends Component {
         ))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+
   render() {
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    // Object destructuring (ES6)
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
     })
 
     return (
       <div className="App" >
         <input className='search-box' type='search' placeholder='search monster'
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase()
-            this.setState(() => {
-              return { searchField }
-            })
-          }} />
+          onChange={onSearchChange} />
 
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            )
-          })
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })
         }
       </div>
     );
