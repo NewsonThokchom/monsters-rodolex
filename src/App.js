e403a9fc742a009295aedbe3f6dd175e657b9fb1
Newson Thokchom <newsonthokchom@gmail.com>
2022-11-21 23:23:29 +0530

    seperate searchbox as component (Searchbox component)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Component } from 'react';
 import Cardlist from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -47,9 +48,6 @@ class App extends Component {
 
     return (
       <div className="App" >
-        <input className='search-box' type='search' placeholder='search monster'
-          onChange={onSearchChange} />
-
         {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
@@ -59,6 +57,7 @@ class App extends Component {
         })
         } */}
 
+        <SearchBox onChangeHandler={onSearchChange} placeholder='Search Monsters' searchBox='Search-box' />
         <Cardlist monsters={filteredMonsters} />
       </div>
     );
