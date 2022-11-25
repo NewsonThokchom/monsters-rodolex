d446fee3844c2633cf27add9fe8d1ee6b3798416
Newson Thokchom <newsonthokchom@gmail.com>
2022-11-25 23:40:49 +0530

    add unique key in card-list card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ const Cardlist = ({ monsters }) => {
         <div className="card-list">
             {monsters.map((monster) => {
                 return (
-                    <Card monster={monster} />
+                    <Card monster={monster} key={monster.id} />
                 )
             })
             }
